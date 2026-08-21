044452fe5b5819f1f58663419813f051a55d3307
RiccardoCoccioni <r.coccioni.3946@vallauri.edu>
2026-08-21 07:30:36 +0200

    cronometro finale

--- LIVELLO_6_Cronometro/script.js
+++ LIVELLO_6_Cronometro/script.js
@@ -1,11 +1,11 @@
 let schermo = document.getElementById("schermo");
-let contatore = 0;
+let millisecondi = 0;
 let cronometro;
 function avviaCronometro() {
     cronometro = setInterval(() => {
-        schermo.textContent = contatore;
-        contatore++;
-    }, 1000);
+        millisecondi += 10;
+        aggiornaSchermo();
+    }, 10);
 }
 
 function fermaCronometro(){
@@ -14,6 +14,24 @@ function fermaCronometro(){
 
 function resetCronometro(){
     clearInterval(cronometro);
-    contatore = 0;
-    schermo.textContent = 0;
+    millisecondi = 0;
+    aggiornaSchermo();
 }
+
+function aggiornaSchermo(){
+    let ore = Math.floor(millisecondi / 3600000);
+    let minuti = Math.floor((millisecondi % 3600000) / 60000);
+    let secondi = Math.floor((millisecondi % 60000) / 1000 );
+    let centesimi = Math.floor((millisecondi % 1000) / 10);
+
+    schermo.textContent = formattaCronometro(ore) + "."  + formattaCronometro(minuti) + "." + formattaCronometro(secondi) + "." + formattaCronometro(centesimi);
+}
+
+function formattaCronometro(numero){
+    if(numero < 10){
+        return "0" + numero;
+    }
+    return numero;
+}
+
+aggiornaSchermo();
